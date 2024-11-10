707fbd2329fdec10cf719426ab9ef67a891c768b
SDY <dylan.stegmuller@heig-vd.ch>
2024-11-10 22:28:13 +0100

    Implemented client part

--- code/gabi3469_9r55hs/Client/src/main/java/ch/heig/dai/lab/protocoldesign/Client.java
+++ code/gabi3469_9r55hs/Client/src/main/java/ch/heig/dai/lab/protocoldesign/Client.java
@@ -1,52 +1,55 @@
 package ch.heig.dai.lab.protocoldesign;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class Client {
     final String SERVER_ADDRESS = "127.0.0.1";
-    final int SERVER_PORT = 1234;
+    final int SERVER_PORT = 42012;
 
     public static void main(String[] args) {
         // Create a new client and run it
-        //Client client = new Client();
-        run();
+        Client client = new Client();
+        client.run();
     }
 
-    private static void run() {
-        try (   Socket socket = new Socket("127.0.0.1", 1234);
+    private void run() {
+        try (   Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
                 var in = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
                 var out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8));
                 var scanner = new Scanner(System.in)) {
-                
-            System.out.println("Sucessfully connected to MOP server!");
+            
+            String serverResponse;
+            for (int i = 0; i < 3; i++) {
+                if (in.ready()) {
+                    serverResponse = in.readLine();
+                    if (serverResponse != null && !serverResponse.isEmpty()) {
+                        System.out.println(serverResponse);
+                    }
+                }
+            }
+
             while (true) {
                 System.out.println("Enter command to send: ");
 
                 String userInput = scanner.nextLine();
                 out.write(userInput + "\n");
                 out.flush();
-                System.out.println("Sent: " + userInput);
-
-                String serverResponse = in.readLine();
+                
+                serverResponse = in.readLine();
                 if (serverResponse != null) {
-                    System.out.println("Received: " + serverResponse);
+                    System.out.println(serverResponse);
+                } else {
+                    System.out.println("Server has closed the connection.");
+                    break;
                 }
+
+                if(userInput.equals("QUIT")) break;
             }
         } catch (IOException e) {
-            System.out.println("Client: exc.: " + e);
-            
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException thException) {
-                thException.printStackTrace();
-            }
+            System.err.println("Client: exc.: " + e.getMessage());
         }
     }
 }
